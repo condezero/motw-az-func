3f80d9faa6f7e8839d58018a3ba483798fb2df78
Carlos Lisandra Osborne <carlos.lisandra@20coolpillows.onmicrosoft.com>
2022-06-16 12:24:04 +0200

    update file path

--- src/mowt-myst-gen/Function1.cs
+++ src/mowt-myst-gen/Function1.cs
@@ -21,7 +21,7 @@ namespace mowt_myst_gen
 
         [FunctionName("Generator")]
         public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ExecutionContext context,
             ILogger log)
         {
 
@@ -34,20 +34,19 @@ namespace mowt_myst_gen
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-
-            string file = await GetHtml();
+            var file = Path.Combine(context.FunctionAppDirectory, "test.html");
+            string html = await GetHtml(file);
             var stubble = new StubbleBuilder().Build();
-            var output = await stubble.RenderAsync(file, new { loc = Data.GetLocation(), mon = Data.GetMonster(), typ = Data.GetMonsterType(), mot = Data.GetMotivation() });
+            var output = await stubble.RenderAsync(html, new { loc = Data.GetLocation(), mon = Data.GetMonster(), typ = Data.GetMonsterType(), mot = Data.GetMotivation() });
             var response = new HttpResponseMessage(HttpStatusCode.OK);
             response.Content = new StringContent(output);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
             return response;
-
         }
 
-        private static async Task<string> GetHtml()
+        private static async Task<string> GetHtml(string file)
         {
-            return await File.ReadAllTextAsync("index.html");
+            return await File.ReadAllTextAsync(file);
         }
         private static string GetMessage() => $"En el/la {Data.GetLocation()} se encuentra {Data.GetMonster()}, con el aspecto {Data.GetMonsterType()} motivada[o] {Data.GetMotivation()}";
 
