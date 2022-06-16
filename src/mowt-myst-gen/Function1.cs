e995bce408e4b4fd0e4b526a629083d6d1c541e4
Carlos Lisandra Osborne <carlos.lisandra@20coolpillows.onmicrosoft.com>
2022-06-16 11:49:39 +0200

    start app

---
+++ src/mowt-myst-gen/Function1.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Stubble.Core.Builders;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace mowt_myst_gen
+{
+    public static class Function1
+    {
+
+
+        [FunctionName("Generator")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+
+            string file = await GetHtml();
+            var stubble = new StubbleBuilder().Build();
+            var output = await stubble.RenderAsync(file, new { loc = Data.GetLocation(), mon = Data.GetMonster(), typ = Data.GetMonsterType(), mot = Data.GetMotivation() });
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(output);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+
+        }
+
+        private static async Task<string> GetHtml()
+        {
+            return await File.ReadAllTextAsync("index.html");
+        }
+        private static string GetMessage() => $"En el/la {Data.GetLocation()} se encuentra {Data.GetMonster()}, con el aspecto {Data.GetMonsterType()} motivada[o] {Data.GetMotivation()}";
+
+    }
+
+}
